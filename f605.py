e464fa3d5169fbd157495b3d1e242806a361bd8c
Jesse <jessie97013@gmail.com>
2022-04-11 17:47:31 +0800

    refactor f605

--- f605.py
+++ f605.py
@@ -1,14 +1,28 @@
 import sys
 
-goods_count, diff = sys.stdin.readline()[:-1].split()
+goods_count, diff = map(int, sys.stdin.readline()[:-1].split())
 
 count = 0
 coast = 0
-for i in range(int(goods_count)):
+for i in range(goods_count):
     prices = [int(x) for x in sys.stdin.readline()[:-1].split()]
-    prices.sort()
-    if prices[-1] - prices[0] >= int(diff):
+    if max(prices) - min(prices) >= diff:
         count += 1
-        coast += int(sum(prices) / 3)
+        coast += sum(prices) // 3
 
 print(count, coast)
+
+# import sys
+#
+# goods_count, diff = sys.stdin.readline()[:-1].split()
+#
+# count = 0
+# coast = 0
+# for i in range(int(goods_count)):
+#     prices = [int(x) for x in sys.stdin.readline()[:-1].split()]
+#     prices.sort()
+#     if prices[-1] - prices[0] >= int(diff):
+#         count += 1
+#         coast += int(sum(prices) / 3)
+#
+# print(count, coast)
